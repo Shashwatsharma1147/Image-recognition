8a3e8e2a101cc14b30d3e3857cafc0fa4fcf5e97
Shashwatsharma1147 <76209043+Shashwatsharma1147@users.noreply.github.com>
2021-12-18 17:31:33 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+Webcam.set({
+    width:350,
+    height:300,
+    image_format:'png',
+    png_quality:90,
+});
+camera = document.getElementById("camera");
+Webcam.attach('#camera');
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'">';
+    });
+}
+console.log("ml5 version="+ml5.version);
+classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/h5G26U5wy/model.json",modelLoaded);
+
+function modelLoaded(){
+    console.log("model is loaded");
+}
+
+function check(){
+    img = document.getElementById("captured_image");
+   classifier.classify(img, getResult);
+}
+
+function getResult(error, results){
+    if (error){
+        console.log(error);
+    }
+    else{
+        console.log(results);
+        document.getElementById("object_result").innerHTML = results[0].label;
+        document.getElementById("accuracy_result").innerHTML = results[0].confidence.toFixed(2);
+    }
+}
